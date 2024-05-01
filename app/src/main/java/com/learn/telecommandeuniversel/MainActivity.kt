ed2f4cc986d01cf72e4924932ae319c41787ff4a
Williams Lenkeu <williams25prince@gmail.com>
2024-05-01 09:08:54 +0100

    DIsposition de la telecommande

--- app/src/main/java/com/learn/telecommandeuniversel/MainActivity.kt
+++ app/src/main/java/com/learn/telecommandeuniversel/MainActivity.kt
@@ -3,14 +3,29 @@ package com.learn.telecommandeuniversel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learn.telecommandeuniversel.ui.theme.TelecommandeUniverselTheme
+import com.learn.telecommandeuniversel.ui.theme.background
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -30,17 +45,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen(){
+    Column(
+        modifier = Modifier
+            .background(background)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(10.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .width(180.dp)
+                .height(270.dp)
+                .padding(10.dp)
+                .background(Color.Magenta)
+        ) {
+            Text(text = "Bouton pour allumer et eteindre")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(270.dp)
+                .padding(10.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TelecommandeUniverselTheme {
-        Greeting("Android")
+                .background(Color.Red)
+        ) {
+            Text(text = "Autres boutons")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(270.dp)
+                .padding(10.dp, 80.dp, 10.dp, 10.dp)
+                .background(Color.Black)
+        ) {
+            Text(text = "Zone de texte pour la frequence")
+        }
     }
 }
